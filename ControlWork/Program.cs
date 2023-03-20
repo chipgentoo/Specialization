c37ac823dcc91fc752a2b473df31984368d8d6a8
chipgentoo <chip.gentoo@gmail.com>
2023-03-20 21:06:22 +0300

    Added word array generator

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string paragraph = "phpMyAdmin is a free software tool written in PHP, intended to handle the administration of MySQL over the Web. phpMyAdmin supports a wide range of operations on MySQL and MariaDB. Frequently used operations (managing databases, tables, columns, relations, indexes, users, permissions, etc) can be performed via the user interface, while you still have the ability to directly execute any SQL statement.";
+
+string[] GetPrgAsArrWord(string str)
+{
+    // удаляем запятые и точки из текста
+    str = str.Replace(",", string.Empty);
+    str = str.Replace(".", string.Empty);
+    return str.Split(' '); // возвращаем в массив слов
+}
+
+System.Console.WriteLine(GetPrgAsArrWord(paragraph));
